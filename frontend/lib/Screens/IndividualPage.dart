c8d56f44a5ca916358064fb6890b5ae0b30791ce
masal9pse <returnymgstokh@icloud.com>
2021-12-31 17:20:49 +0900

    #9 input card of bottom form

--- frontend/lib/Screens/IndividualPage.dart
+++ frontend/lib/Screens/IndividualPage.dart
@@ -13,6 +13,7 @@ class _IndividualPageState extends State<IndividualPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.blueGrey,
       appBar: AppBar(
         leadingWidth: 70,
         leading: InkWell(
@@ -63,6 +64,56 @@ class _IndividualPageState extends State<IndividualPage> {
             },
           ),
         ],
+      ),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Stack(
+          children: [
+            ListView(),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Row(
+                children: [
+                  Container(
+                    width: MediaQuery.of(context).size.width - 55,
+                    child: Card(
+                      margin: EdgeInsets.only(left: 2,right: 2,bottom: 8),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(25)
+                      ),
+                      child: TextFormField(
+                        textAlignVertical: TextAlignVertical.center,
+                        keyboardType: TextInputType.multiline,
+                        maxLines: 5,
+                        minLines: 1,
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: 'Type a message',
+                          prefixIcon: IconButton(onPressed: (){}, icon: Icon(Icons.emoji_emotions)),
+                          suffixIcon: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: <Widget>[
+                              IconButton(onPressed: null, icon: Icon(Icons.attach_file)),
+                              IconButton(onPressed: null, icon: Icon(Icons.camera_alt)),
+                            ],
+                          )
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 8,right: 2),
+                    child: CircleAvatar(
+                      radius: 25,
+                      child: IconButton(onPressed: null, icon: Icon(Icons.mic)),
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
