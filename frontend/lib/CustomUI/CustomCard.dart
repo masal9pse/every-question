96c966849943f141035def8d09245b3a0f2bb0c5
masal9pse <returnymgstokh@icloud.com>
2021-12-31 12:47:54 +0900

    #4 using ListView,ListTile

---
+++ frontend/lib/CustomUI/CustomCard.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class CustomCard extends StatefulWidget {
+  const CustomCard({Key? key}) : super(key: key);
+
+  @override
+  _CustomCardState createState() => _CustomCardState();
+}
+
+class _CustomCardState extends State<CustomCard> {
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: CircleAvatar(
+        radius: 30,
+      ),
+      title: Text(
+        'dev stack',
+        style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+      ),
+      subtitle: Row(
+        children: [
+          Icon(Icons.done_all),
+          SizedBox(
+            width: 3,
+          ),
+          Text(
+            'Hi dev stack',
+            style: TextStyle(fontSize: 14),
+          )
+        ],
+      ),
+    );
+  }
+}
--- frontend/lib/Pages/ChatPage.dart
+++ frontend/lib/Pages/ChatPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/CustomUI/CustomCard.dart';
 
 class ChatPage extends StatefulWidget {
   const ChatPage({ Key? key }) : super(key: key);
@@ -11,7 +12,16 @@ class _ChatPageState extends State<ChatPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        child: Icon(Icons.chat),
+      ),
+      body: ListView(
+        children: [
+          CustomCard(),
+          CustomCard()
+        ],
+      ),
     );
   }
 }
